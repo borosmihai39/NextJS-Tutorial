b884de286170183258039c4a41fe8f0e8a123600
mihboros <mihai.boros@tremend.com>
2024-03-01 17:48:34 +0200

    added the prisma client

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,3 +1,4 @@
+import DrinksList from "@/components/DrinksList";
 import React from "react";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
@@ -14,11 +15,10 @@ const fetchDrinks = async () => {
 
 const DrinksPage = async () => {
   const data = await fetchDrinks();
-  console.log(data);
 
   return (
     <div>
-      <h1 className="text-7xl">Drinks Page</h1>
+      <DrinksList drinks={data.drinks} />
     </div>
   );
 };
